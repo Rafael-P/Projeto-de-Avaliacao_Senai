0445d29c2a4b8bc9a8d2445793499f3b8c2bd6c3
Rafael-P <r4f4el_porto@hotmail.com>
2021-01-27 11:29:44 -0300

    remocao do back

--- Models/InstaBase.cs
+++ Models/InstaBase.cs
@@ -6,17 +6,6 @@ namespace Projeto_de_Avaliacao_Senai.Models
     public class InstaBase
     {
         
-        public void RewriteCSV(string path, List<string> linhas)
-        {
-            //StreamWriter -> escreve dados em um arquivo
-            using(StreamWriter output = new StreamWriter(path))
-            {
-                foreach (var item in linhas)
-                {
-                    output.Write(item + '\n');
-                }
-            }
-        }
         
 
     }
