5398e74f5c152f7f5d15e6aa8b3e70ffd88ac662
Flavia-OlSi <flavia_gudy@hotmail.com>
2021-01-29 02:03:21 -0300

    Visual finalizado, faltando apenas funções

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function deslike()
+{
+    document.getElementById("alterar-icone").src="../Feed/img/Like.svg";
+}
+
+function like()
+{
+    document.getElementById("alterar-icone").src="../Feed/img/Like-vermelho.svg";
+}
