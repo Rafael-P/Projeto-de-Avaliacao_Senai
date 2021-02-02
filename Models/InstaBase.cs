00deb615be5679eb5d3b4d23b61fdc120c835d4a
Flavia-OlSi <64510694+Flavia-OlSi@users.noreply.github.com>
2021-02-01 21:04:48 -0300

    Revert "Page/feed"

--- Models/InstaBase.cs
+++ Models/InstaBase.cs
@@ -6,5 +6,7 @@ namespace Projeto_de_Avaliacao_Senai.Models
     public class InstaBase
     {
         
+        
+
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-function like()
-{
-    if(document.getElementById("amei").checked == false){
-        document.getElementById("alterar-icone").src="../Feed/img/Like-vermelho.svg";
-    }else{
-        document.getElementById("alterar-icone").src="../Feed/img/Like.svg";
-    }
-}
