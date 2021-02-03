63c3ae5b72ff2263c4d3525bb0c88527a431b065
GABRIELA_GONZAGA <biapougica@gmail.com>
2021-02-03 19:42:04 -0300

    Direções -cadastro

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Projeto_de_Avaliacao_Senai.Controllers
 {
-    public class CadastroController
+    public class CadastroController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         
     }
 }
