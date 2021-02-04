e5d53716752de146dc7292d7e13e42ecdf1d5681
Rafael-P <r4f4el_porto@hotmail.com>
2021-02-04 20:22:20 -0300

    adc controller edicao

--- Controllers/EdicaoController.cs
+++ Controllers/EdicaoController.cs
@@ -1,16 +1,38 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Projeto_de_Avaliacao_Senai.Models;
 
 namespace Projeto_de_Avaliacao_Senai.Controllers
 {
     [Route("EdicaoPerfil")]
     public class EdicaoController : Controller
     {
+        Usuario usuarioModel = new Usuario();
 
         public IActionResult Index()
         {
 
             return View();
             
+        }
+
+        [Route("DeletarConta")]
+        public IActionResult DeletarUsuario(int id)
+        {
+            usuarioModel.DeletarUsuario(id);
+            ViewBag.Usuario = usuarioModel.ListarUsuario();
+
+            return LocalRedirect("~/EdicaoPerfil");
+        }
+
+        [Route("EditarConta")]
+        public IActionResult EditarUsuario(Usuario u)
+        {
+            usuarioModel.EditarUsuario(u);
+            // ViewBag.Usuario = usuarioModel.RewriteCSV(string path, List<string> linhas);
+
+            return LocalRedirect("~/EdicaoPerfil");
         }
 
 
