8cd6ca164d91f16bc37dce43b4131995aabe9c6f
Flavia-OlSi <flavia_gudy@hotmail.com>
2021-02-03 23:22:41 -0300

    Validar

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -16,6 +16,45 @@ namespace Projeto_de_Avaliacao_Senai.Models
         public string Name { get; set; }
         public string Username { get; set; }
         public string Senha { get; set; }
-        
+
+        public void CadastrarUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletarUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> ListarUsuario()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Logar(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario MostrarUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Prepare(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Seguir(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
