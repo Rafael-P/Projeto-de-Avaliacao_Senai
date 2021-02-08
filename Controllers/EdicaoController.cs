e4fd95d95d553f12bd65152688c83e6bc57412e0
gabriela gonzaga <biapougica@gmail.com>
2021-02-07 22:48:05 -0300

    Foto funcionando amém

--- Controllers/EdicaoController.cs
+++ Controllers/EdicaoController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Projeto_de_Avaliacao_Senai.Models;
@@ -48,8 +49,40 @@ namespace Projeto_de_Avaliacao_Senai.Controllers
             editarUsuario.Nome      = form["Nome"];
             editarUsuario.Username  = form["Username"];
             editarUsuario.Email     = form["Email"];
-            editarUsuario.Foto      =form["Foto"];
-            
+            editarUsuario.Foto      = form["Foto"];
+            editarUsuario.IdUsuario  =int.Parse(form["Id"]); 
+             
+
+              if(form.Files.Count > 0)
+            {
+                /*Variavel que armazena o arquivo enviada pelo usuário*/
+                var file = form.Files[0];
+                /*Essa varivel vai combinar (.Combine) o diretorio com a pasta*/
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/");
+
+                /*Condicional para ver se pasta "wwwroot/img/" já existe*/
+                if(!Directory.Exists(folder))
+                {
+                    /*folder é a variavel que está ali em cima*/
+                    Directory.CreateDirectory(folder);
+                }
+
+                /*Variavel para definir o caminho da criação do arquivo*/
+                                         //localhost:5001                                //Post   //imagem.jpg
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                /*Utilizada para fechar automaticamente*/
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                editarUsuario.Foto = file.FileName;
+            }
+            else
+            {
+                editarUsuario.Foto = form["Foto2"];
+            }
 
             usuarioModel.EditarUsuario(editarUsuario);
 
